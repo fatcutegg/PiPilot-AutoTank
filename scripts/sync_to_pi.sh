9875a5572e5b6d8b596d1e918a6aa80beabb1d9b
!am9a09e <gg9a09e@gmail.com>
2026-05-08 00:38:48 +0900

    chore: update pi sync script with new project name

--- scripts/sync_to_pi.sh
+++ scripts/sync_to_pi.sh
@@ -2,6 +2,6 @@
 # Mac から 樹莓派 へコードを同期するスクリプト (Rsync Sync Script)
 # Type-C OTG で接続されている 'tank' エイリアスを使用します。
 
-echo "Syncing code to Raspberry Pi (tank)..."
-rsync -avz --exclude '.git' --exclude '__pycache__' ./ tank:~/tank_project/
+echo "Syncing code to Raspberry Pi (PiPilot-AutoTank)..."
+rsync -avz --exclude '.git' --exclude '__pycache__' --exclude 'dataset' ./ tank:~/PiPilot-AutoTank/
 echo "Sync completed at $(date)"
